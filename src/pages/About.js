0b5f83a7264a4afda2204750cb7881c66e01d60f
marc hudson <marchudson2601@gmail.com>
2023-03-04 01:35:48 +0000

    added new styling to about page and new structure to the about me html

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,63 +9,44 @@ export default () => {
                 <title>Marc Hudson - About Me</title>
             </Helmet>
 
-            <section className="abouthero ">
-                <div>
+            <section className="abouthero">
+                <div className="ornament">
                     <h2>
-                        Marc Hudson
+                        About
                     </h2>
-                    <h1>Front end Web Engineer </h1>
+                    <h1>Marc Hudson</h1>
+                </div>
+                <div>
+                    <img src="/aboutheadimag.png" />
                 </div>
             </section>
             <main class="main ">
-                <h3>About Me</h3>
                 <div class="slide">
-                    <p>Hello, I'm Marc. Up until recently I was a HGV driver, I have recently discovered my love for coding.
-                        I
-                        started my
-                        journey to become a front-end web developer in October 2022. I decided to pursue my interest in
-                        programming
-                        with help of family members who are software engineers. I have dedicated my spare time to learning
-                        these
-                        new skills.
+                    <p>As the clattering of steam-powered machinery echoes through the air, a figure emerges from the mist, a web engineer Marc Hudson with a unique set of skills and a passion for innovation.
                     </p>
-                    <p>Throughout this experience so far I have enjoyed paying attention to the small details. I am
-                        currently
-                        enrolled in a skills
-                        bootcamp into front-end web development. I'm fairly new to programming but spend as much of my free
-                        time
-                        researching JavaScript and HTML/CSS as these particularly interest me.
+                    <p>Born into a world of gears and cogs, once trained as a soldier, and now father to 6 children, this engineer learned the intricacies of web development later in life. They are honing their skills in the fiery furnace of the steam-powered internet, and hope to one day emerged a master of their craft.
                     </p>
-                    <p>I like using CSS to style pages and make them user friendly. I am constantly learning new techniques
-                        to improve my work; in my projects below you can see my portfolio Vol 1 which I look at to just
-                        remind
-                        me how far I have come in this short time period.
-                    </p>
-                    <p>My ambition is to eventually become a lead front-end engineer. It will take a lot of time, however, I
-                        will not
-                        stop putting the effort and time in until I reach my goal. I have pet projects to experiment with as
-                        well as
-                        pair-programmes to try understand how best to work in a team of developers. This helps me gain
-                        insight
-                        into
-                        good programming practices.
-                    </p>
-                    <p>I hope to eventually solve real-world problems using technology, and finally kick-start a career in
-                        tech.
+                    <p>But it's not just about the code. This engineer understands the importance of user experience, and is constantly seeking out new ways to create engaging and intuitive interfaces. They are constantly resurrecting new trends and technologies, always striving to push the boundaries of what's possible.
                     </p>
                 </div>
             </main>
+            <section className="cvcta">
+
+            </section>
+
+
+
             <section className="aboutlower">
                 <div>
-                    <img src="/vector-svgrepo-com.svg"></img>
+
                     <h3>Web Design</h3>
                 </div>
                 <div>
-                    <img src="/phone-screen.svg"></img>
+
                     <h3>Responsive Design</h3>
                 </div>
                 <div>
-                    <img src="/laptop-512.png"></img>
+
                     <h3>Web Developer</h3>
                 </div>
             </section>
