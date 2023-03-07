732885852d511988d8eaacea4d6e2ff3788f87f4
marc hudson <marchudson2601@gmail.com>
2023-03-07 01:20:24 +0000

    adding meter component

---
+++ src/components/Meter/index.js
@@ -0,0 +1,108 @@
+import { color } from "d3-color";
+import { interpolateRgb } from "d3-interpolate";
+import LiquidFillGauge from "react-liquid-gauge";
+
+
+
+const Meter = ({ value, name }) => {
+
+    const startColor = "blu";
+    const endColor = "gray";
+
+
+    const radius = 100;
+    const interpolate = interpolateRgb(startColor, endColor);
+    const fillColor = interpolate(value / 100);
+    const gradientStops = [
+        {
+            key: "0%",
+            stopColor: color(fillColor)
+                .darker(0.5)
+                .toString(),
+            stopOpacity: 1,
+            offset: "0%"
+        },
+        {
+            key: "50%",
+            stopColor: fillColor,
+            stopOpacity: 0.75,
+            offset: "50%"
+        },
+        {
+            key: "100%",
+            stopColor: color(fillColor)
+                .brighter(0.5)
+                .toString(),
+            stopOpacity: 0.5,
+            offset: "100%"
+        }
+    ];
+
+    return (
+        <div>
+            <LiquidFillGauge
+                style={{ margin: "0 auto" }}
+                width={radius * 2}
+                height={radius * 2}
+                value={value}
+                percent="%"
+                textSize={0.9}
+                textOffsetX={0}
+                textOffsetY={0}
+                textRenderer={props => {
+                    const value = Math.round(props.value);
+                    const radius = Math.min(props.height / 2, props.width / 2);
+                    const textPixels = (props.textSize * radius) / 2;
+                    const valueStyle = {
+                        fontSize: textPixels
+                    };
+                    const percentStyle = {
+                        fontSize: textPixels * 0.6
+                    };
+
+                    return (
+
+                        <>
+                            <tspan dy="0rem" x="0">
+                                <tspan className="value" style={valueStyle}>
+                                    {value}
+                                </tspan>
+                                <tspan style={percentStyle}>{props.percent}</tspan>
+                            </tspan>
+
+                            <tspan dy="2.8rem" x="0" style={valueStyle}>
+                                {name}
+                            </tspan>
+                        </>
+
+
+                    );
+                }}
+                riseAnimation
+                waveAnimation
+                waveFrequency={2}
+                waveAmplitude={1}
+                gradient
+                gradientStops={gradientStops}
+                circleStyle={{
+                    fill: fillColor
+                }}
+                waveStyle={{
+                    fill: fillColor
+                }}
+                textStyle={{
+                    fill: color("#444").toString(),
+                    fontFamily: "Arial"
+                }}
+                waveTextStyle={{
+                    fill: color("#fff").toString(),
+                    fontFamily: "Arial"
+                }}
+
+            />
+
+        </div>
+    );
+
+}
+export default Meter
