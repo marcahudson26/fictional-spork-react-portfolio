0cd992351e55488da378bf5f8cd99969d6ec0375
marc hudson <marchudson2601@gmail.com>
2023-03-07 01:22:51 +0000

    updating component styles and fixing navigation

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,11 +1,36 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./index.scss";
+import React from "react";
+
+const pages = [{
+    name: "Home",
+    link: "/",
+},
+{
+    name: "About",
+    link: "/about",
+},
+{
+    name: "Skills",
+    link: "/skills",
+},
+{
+    name: "Projects",
+    link: "/projects",
+},
+]
 
 export default () => {
+    const [isOpen, setIsOpen] = React.useState(false)
+    const location = useLocation()
+    const menus = pages.filter(page => {
+        return page.link !== location.pathname
+    })
+
     return (
         <>
-            <input type="checkbox" className="hide" id="menu-trigger" role="button" title="menu" />
-            <label htmlFor="menu-trigger" aria-hidden="true" title="menu">
+            <input type="checkbox" checked={isOpen} className="hide" id="menu-trigger" role="button" title="menu" />
+            <label htmlFor="menu-trigger" aria-hidden="true" title="menu" onClick={() => setIsOpen(!isOpen)}>
                 <span className="burger">
                     <span className="bar">
                         <span className="visually-hidden">Menu</span>
@@ -13,12 +38,13 @@ export default () => {
                 </span>
             </label>
             <nav id="menu">
-                <Link to="/about">About Me</Link>
-                <a href="#">My Projects</a>
-                <Link to="/skills">My Skills</Link>
-                <a href="#">Contact Me</a>
-                <a href="https://github.com/marcahudson26">My Github</a>
-                <a href="https://www.linkedin.com/in/marc-hudson-53546a259/">LinkedIn</a>
+                {menus.map(menu => (
+                    <Link onClick={() => setIsOpen(!isOpen)} to={menu.link}>{menu.name}</Link>
+                )
+                )}
+                <a href="mailto:marchudson2601@gmail.com" >Contact Me 📧</a>
+                <a href="https://github.com/marcahudson26" target="blank">My Github</a>
+                <a href="https://www.linkedin.com/in/marc-hudson-53546a259/" target="blank">LinkedIn</a>
             </nav>
         </>
     )
